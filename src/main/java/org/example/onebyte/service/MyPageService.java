fcb69476c5dcf9dce91193043171a7cf926cc50a
허보미 <huhbomi@gmail.com>
2025-12-25 11:37:56 +0900

    feat : a ì내정보ì¡비밀번호 변경

--- src/main/java/org/example/onebyte/service/MyPageService.java
+++ src/main/java/org/example/onebyte/service/MyPageService.java
@@ -2,8 +2,15 @@ package org.example.onebyte.service;
 
 import org.example.onebyte.dto.mypage.MyPageInfoResponse;
 import org.example.onebyte.dto.mypage.UpdateInfoRequest;
+import org.example.onebyte.dto.mypage.UpdatePasswordRequest;
 
 public interface MyPageService {
+    //정보조회
     MyPageInfoResponse getInfo(Long userId);
+
+    //이름, 닉네임 변경
     void updateInfo(Long userId, UpdateInfoRequest request);
+
+    //비밀번호 변경
+    void updatePassword(Long userId, UpdatePasswordRequest request);
 }
--- src/main/java/org/example/onebyte/service/MyPageServiceImpl.java
+++ src/main/java/org/example/onebyte/service/MyPageServiceImpl.java
@@ -3,10 +3,12 @@ package org.example.onebyte.service;
 import lombok.RequiredArgsConstructor;
 import org.example.onebyte.dto.mypage.MyPageInfoResponse;
 import org.example.onebyte.dto.mypage.UpdateInfoRequest;
+import org.example.onebyte.dto.mypage.UpdatePasswordRequest;
 import org.example.onebyte.entity.User;
 import org.example.onebyte.exception.AuthenticationFailedException;
 import org.example.onebyte.exception.DuplicateResourceException;
 import org.example.onebyte.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +17,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class MyPageServiceImpl implements MyPageService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    //정보조회
     @Override
     @Transactional(readOnly = true)
     public MyPageInfoResponse getInfo(Long userId) {
@@ -24,6 +28,7 @@ public class MyPageServiceImpl implements MyPageService {
         return MyPageInfoResponse.from(user);
     }
 
+    //이름, 닉네임변경
     @Override
     @Transactional
     public void updateInfo(Long userId, UpdateInfoRequest request){
@@ -46,8 +51,23 @@ public class MyPageServiceImpl implements MyPageService {
         user.changeInfo(newName, newNickname);
     }
 
+    //비밀번호 변경
+    @Override
+    @Transactional
+    public void updatePassword(Long userId, UpdatePasswordRequest req) {
 
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new AuthenticationFailedException("존재하지 않는 사용자 입니다."));
 
+        if (!passwordEncoder.matches(req.getCurrentPassword(), user.getPasswordHash())) {
+            throw new AuthenticationFailedException("현재 비밀번호가 일치하지 않습니다.");
+        }
 
+        if (passwordEncoder.matches(req.getNewPassword(), user.getPasswordHash())) {
+            throw new IllegalArgumentException("기존 비밀번호와 동일합니다.");
+        }
 
+        String newPasswordHash = passwordEncoder.encode(req.getNewPassword());
+        user.changePasswordHash(newPasswordHash);
+    }
 }

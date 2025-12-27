5c7456a7cd4532e4063c67f2f0eb474bab88a710
jeongbeomgyu <jeongbeomgyu@github.com>
2025-12-27 17:29:45 +0900

    삭제된 카테고리의 글 기타 카테고리로 이동 Query 작성

--- src/main/java/org/example/onebyte/repository/BoardRepository.java
+++ src/main/java/org/example/onebyte/repository/BoardRepository.java
@@ -1,4 +1,23 @@
 package org.example.onebyte.repository;
 
-public interface BoardRepository {
+import org.example.onebyte.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    // 카테고리 삭제할 때, 삭제한 카테고리의 Id 를 강제로 교체 시킴
+    @Modifying
+    @Query("UPDATE Board b SET b.category.id = :etcId WHERE b.category.id = :targetId")
+    void updateCategoryBatch(@Param("targetId") Long targetId, @Param("etcId") Long etcId);
+
+    // 삭제된 카테고리의 글들을 숨기는 쿼리문
+    @Query("SELECT b FROM Board b JOIN b.category c WHERE c.isActive = true")
+    Page<Board> findAllVisibleBoards(Pageable pageable);
+
+
 }
